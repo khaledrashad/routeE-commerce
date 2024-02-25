1704d73ef2d685514aa0c626b669a8e0b840fc4e
amr rashad <khaledamr400@gmail.com>
2024-02-25 17:52:37 +0200

    home sliders done

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -6,15 +6,45 @@ import { Link, useNavigate } from 'react-router-dom'
 import { useQuery } from 'react-query'
 import { CartContext } from '../../Context/CartContext.js'
 import toast from 'react-hot-toast'
+import Slider from 'react-slick'
+import slider1 from "../../Assets/images/slider-image-1.jpeg"
+import slider2 from "../../Assets/images/slider-image-2.jpeg"
+import slider3 from "../../Assets/images/slider-image-3.jpeg"
+import sidePic1 from "../../Assets/images/grocery-banner.png"
+import sidePic2 from "../../Assets/images/grocery-banner-2.jpeg"
 
 export default function Home() {
 
-  let { addToCart , setCart} = useContext(CartContext)
+  var settings = {
+    dots: false,
+    infinite: true,
+    speed: 1000,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    arrows: false
+  }
+
+  const setting1 = {
+    className: "center",
+    centerMode: true,
+    infinite: true,
+    centerPadding: "60px",
+    slidesToShow: 5,
+    speed: 500,
+    rows: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    slidesPerRow: 1
+  };
+
+  let { addToCart, setCart } = useContext(CartContext)
 
   async function PostDataToCart(id) {
     let { data } = await addToCart(id)
     if (data.status == "success") {
-      toast.success(data.message , {icon: '👏', })
+      toast.success(data.message, { icon: '👏', })
     } else {
       toast.error("Error in adding product")
     }
@@ -25,8 +55,51 @@ export default function Home() {
 
   }
   let { data, isLoading, isFetched, isError } = useQuery("homeData", getHomeData)
+  async function getCatData() {
+    return await axios.get("https://route-ecommerce.onrender.com/api/v1/categories")
+
+  }
+  let catData = useQuery("catData", getCatData)
 
   return <>
+
+    <div className="row py-5 align-items-lg-stretch">
+      <div className="col-md-8 p-0">
+        <Slider {...settings}>
+          <div>
+            <img src={slider1} alt="" className='w-100' />
+          </div>
+          <div>
+            <img src={slider2} alt="" className='w-100' />
+          </div>
+          <div>
+            <img src={slider3} alt="" className='w-100' />
+          </div>
+        </Slider>
+      </div>
+      <div className="col-md-4 p-0">
+        <img src={sidePic1} alt="" className='w-100 h-50' />
+        <img src={sidePic2} alt="" className='w-100 h-50 pb-1' />
+      </div>
+    </div>
+
+    <h2 className='fw-bold'>Shop By Category:</h2>
+
+    {isLoading ? <div className='vh-100 d-flex justify-content-center align-items-center'><DNA
+      visible={true}
+      height="200"
+      width="200"
+      ariaLabel="dna-loading"
+      wrapperStyle={{}}
+      wrapperClass="dna-wrapper"
+    /></div> : <div className="slider-container py-5">
+      <Slider {...setting1}>
+        {catData?.data.data.data.map(cat => <div key={cat._id}>
+          <img src={cat.image} alt="" className='w-100 h-30' />
+        </div>)}
+      </Slider>
+    </div>}
+
     {isLoading ? <div className='vh-100 d-flex justify-content-center align-items-center'><DNA
       visible={true}
       height="200"
@@ -51,7 +124,7 @@ export default function Home() {
                   </div>
                 </div>
               </Link>
-              <button onClick={() => { PostDataToCart(product.id)}} className='btn bg-main w-100'>Add to cart</button>
+              <button onClick={() => { PostDataToCart(product.id) }} className='btn bg-main w-100'>Add to cart</button>
             </div>
           </div>)}
       </div>
